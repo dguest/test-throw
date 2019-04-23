43744e4bd9d20a0f1228a1b3a30562250edb98fb
Dan Guest <dguest@cern.ch>
2019-04-23 21:23:16 +0200

    remove useless stuff

--- util/test-throw.cxx
+++ util/test-throw.cxx
@@ -1,9 +1,4 @@
 
-// AnalysisBase tool include(s):
-#include "xAODRootAccess/Init.h"
-#include "xAODRootAccess/TEvent.h"
-#include "xAODRootAccess/tools/ReturnCheck.h"
-
 // stl includes
 #include <stdexcept>
 #include <string>
@@ -16,8 +11,4 @@ int main (int, char *[])
 {
   throw std::runtime_error("broken");
   return 0;
-  // set up xAOD basics
-  // RETURN_CHECK(ALG, xAOD::Init());
-  // xAOD::TEvent event(xAOD::TEvent::kClassAccess);
-
 }
